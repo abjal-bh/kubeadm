a839176c148aaab9bdb10e4b01f93ec817e92405
abjal-devops <66158379+abjal-devops@users.noreply.github.com>
2020-09-05 16:01:29 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,30 @@
+# required two input values $1 $2
+yum install docker -y
+systemctl enable docker 
+systemctl start docker 
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet
+systemctl start kubelet
+hostnamectl set-hostname master-node
+echo $1 >> /etc/hosts
+echo $2 >> /etc/hosts
+# hostnamectl set-hostname slave-1
+# hostnamectl set-hostname slave-2
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+############## till above can be used for slave node ############
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
